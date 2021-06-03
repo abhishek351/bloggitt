c7496ef0ce895893219c0ca90d66d0b5a7002a31
abhishek <abhishek291013@gmail.com>
2021-06-03 17:47:34 +0530

    canges

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -152,5 +152,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
