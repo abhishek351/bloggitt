808f300ffbb3d58a039ed693a6ec95b2e5b1033d
abhishek <abhishek291013@gmail.com>
2021-06-03 23:20:48 +0530

    updated

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -29,12 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&a3k2f_s^f-chj*h)82g^@65d#r#%c!e2=_w4a(nqfqcnp4+p)'
+SECRET_KEY = ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bloggitt.herokuapp.com']
+ALLOWED_HOSTS = ['bloggitt.herokuapp.com','127.0.0.1']
 
 
 
