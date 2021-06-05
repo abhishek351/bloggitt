f2788440c4687a5e2a7ffc8645844c57a89d01fb
abhishek <abhishek291013@gmail.com>
2021-06-05 13:56:34 +0530

    chnges in nsetting

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 import dj_database_url 
+from decouple import config
 
 
 
@@ -152,3 +154,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
