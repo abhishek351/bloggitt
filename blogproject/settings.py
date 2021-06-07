171783c3d0e85620a5478456f8cec6c7bb3cd1ca
abhishek <abhishek291013@gmail.com>
2021-06-07 22:36:40 +0530

    update my dtabase

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -90,12 +90,24 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogproject',
+        'USER': 'postgres',
+        'PASSWORD': 'abhi@123',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
+
 
 
 # Password validation
