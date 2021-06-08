ac6411eafa57395aee34ba8e3d9ce6952fc15b5e
abhishek <abhishek291013@gmail.com>
2021-06-08 22:06:42 +0530

    database

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -97,8 +97,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 # # DATABASES = {
 # #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
