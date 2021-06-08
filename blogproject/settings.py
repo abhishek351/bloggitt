492b8d4a47d8828aeded86fa751a4e2e860fb49f
abhishek <abhishek291013@gmail.com>
2021-06-08 21:54:21 +0530

    commit

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -97,10 +97,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-# DATABASES = {
-#     'default': {
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
+# # DATABASES = {
+# #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': 'blogproject',
 #         'USER': 'postgres',
